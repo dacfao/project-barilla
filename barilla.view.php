a3ed4b4c4fab82e5b5315bf2896bdd190bd5582d
dacfao <159134052+dacfao@users.noreply.github.com>
2025-02-28 20:37:31 +0100

    Create barilla.view.php
     front end

---
+++ barilla.view.php
@@ -0,0 +1,98 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="barilla.css" rel="stylesheet" />
+
+    <title>Boutons de Pâtes</title>
+</head> 
+<body>
+  <div class="titre">
+  <h1>Projet Barilla </h1>
+
+</div>
+  <div class="texte">
+  <h2> Choisissez quel paquet de pate vous voulez cuisinez </h2>
+
+</div>
+<img
+  class="logobarilla"
+  src="file:///E:/projet%20sosso/logobarilla.jpg"
+  alt="logo de la marque barilla" />
+  <div class="Fusilli Al Bronzo"><input type="button" value="Fusilli Al Bronzo"></div>
+        <div class="Fusilloni Al Bronzo"><input type="button" value="Fusilloni Al Bronzo"></div>
+        <div class="Linguine Al Bronzo"><input type="button" value="Linguine Al Bronzo"></div>
+        <div class="Mezzi Rigatoni Al Bronzo"><input type="button" value="Mezzi Rigatoni Al Bronzo"></div>
+        <div class="Penne Rigate Al Bronzo"><input type="button" value="Penne Rigate Al Bronzo"></div>
+        <div class="Spaghetti Al Bronzo"><input type="button" value="Spaghetti Al Bronzo"></div>
+        <div class="Spaghettoni Al Bronzo"><input type="button" value="Spaghettoni Al Bronzo"></div>
+        <div class="Coquillettes blé complet"><input type="button" value="Coquillettes blé complet"></div>
+        <div class="Farfalle Integrale"><input type="button" value="Farfalle Integrale"></div>
+        <div class="Fusilli Blé Complet"><input type="button" value="Fusilli Blé Complet"></div>
+        <div class="Linguine Blé Complet"><input type="button" value="Linguine Blé Complet"></div>
+        <div class="Penne Rigate Blé Complet"><input type="button" value="Penne Rigate Blé Complet"></div>
+        <div class="Spaghetti Blé Complet"><input type="button" value="Spaghetti Blé Complet"></div>
+        <div class="Tortiglioni Blé Complet"><input type="button" value="Tortiglioni Blé Complet"></div>
+        <div class="Cannelloni"><input type="button" value="Cannelloni"></div>
+        <div class="Casarecce"><input type="button" value="Casarecce"></div>
+        <div class="Castellane"><input type="button" value="Castellane"></div>
+        <div class="Conchiglie"><input type="button" value="Conchiglie"></div>
+        <div class="Farfalle Generose"><input type="button" value="Farfalle Generose"></div>
+        <div class="Fettuccine"><input type="button" value="Fettuccine"></div>
+        <div class="Fusilli Bucati Corti"><input type="button" value="Fusilli Bucati Corti"></div>
+        <div class="Lasagne con Spinaci"><input type="button" value="Lasagne con Spinaci"></div>
+        <div class="Lasagne semola Collezione"><input type="button" value="Lasagne semola Collezione"></div>
+        <div class="Mafaldine"><input type="button" value="Mafaldine"></div>
+        <div class="Mezze Penne Tricolore"><input type="button" value="Mezze Penne Tricolore"></div>
+        <div class="Orecchiette"><input type="button" value="Orecchiette"></div>
+        <div class="Tagliatelle"><input type="button" value="Tagliatelle"></div>
+        <div class="Tagliatelle aux Épinards"><input type="button" value="Tagliatelle aux Épinards"></div>
+        <div class="Tortellini au Jambon et au Fromage"><input type="button" value="Tortellini au Jambon et au Fromage"></div>
+        <div class="Treccin"><input type="button" value="Treccin"></div>
+        <div class="Trofie"><input type="button" value="Trofie"></div>
+        <div class="Fettuccine à l'œuf"><input type ="button" value="Fettuccine à l'œuf"></div>
+        <div class="Fettuccine Ricce à l'œuf"><input type="button" value="Fettuccine Ricce à l'œuf"></div>
+        <div class="Lasagne à l'œuf"><input type="button" value ="Lasagne à l'œuf"></div>
+        <div class="Pappardelle à l'œuf"><input type ="button" value="Pappardelle à l'œuf"></div>
+        <div class="Tagliatelle à l'œuf"><input type ="button" value="Tagliatelle à l'œuf"></div>
+        <div class ="Casarecce de Pois Chiche"><input type="button" value="Casarecce de Pois Chiche"></div>
+        <div class="Fusilli de lentilles corail"><input type="button" value="Fusilli de lentilles corail"></div>
+        <div class=  "Risoni aux Lentilles Corail et aux Pois Chiches"><input type="button" value=  "Risoni aux Lentilles Corail et aux Pois Chiches"></div>
+        <div class=  "Risoni aux Lentilles Corail, aux Pois Chiches et aux Petits Pois"><input type="button" value=  "Risoni aux Lentilles Corail, aux Pois Chiches et aux Petits Pois"></div>
+        <div class="Spaghetti de Lentilles Corail"><input type="button" value="Spaghetti de Lentilles Corail"></div>
+        <div class=  "Bavette"><input type ="button" value=  "Bavette"></div>
+        <div class="Capellini N°1"><input type="button" value="Capellini N°1"></div>
+        <div class=  "Ditalini Rigati"><input type="button " value=  "Ditalini Rigati"></div>
+        <div class =  "Filini"><input type="button" value=  "Filini"></div>
+        <div class=  "Fusilli"><input type ="button" value= "Fusilli"></div>
+        <div class="Girandole Torsades"><input type ="button" value="Girandole Torsades"></div>
+        <div class="Gnocchi"><input type="button" value="Gnocchi"></div>
+        <div class =  "Maccheroni"><input type ="button" value=  "Maccheroni"></div>
+        <div class= "Penne Lisce"><input type="button" value= "Penne Lisce"></div>
+        <div class= "Penne Rigate"><input type="button" value= "Penne Rigate"></div>
+        <div class= "Pipe Rigate"><input type="button" value= "Pipe Rigate"></div>
+        <div class=  "Rigatoni"><input type="button" value=  "Rigatoni"></div>
+        <div class = "Risoni"><input type ="button" value= "Risoni"></div>
+        <div class="Spaghetti No. 5"><input type="button" value="Spaghetti No. 5"></div>
+        <div class= "Spaghettini N°3"><input type="button" value = "Spaghettini N°3"></div>
+        <div class=  "Spaghettoni N°7"><input type ="button" value=  "Spaghettoni N°7"></div>
+        <div class= "Stelline"><input type="button" value= "Stelline"></div>
+        <div class= "Mini Farfalle"><input type="button" value= "Mini Farfalle"></div>
+        <div class="Mini Penne Rigate"><input type="button" value ="Mini Penne Rigate"></div>
+        <div class=  "Mini Pipe Rigate"><input type="button" value=  "Mini Pipe Rigate"></div>
+        <div class ="Coquillettes Sans Gluten"><input type ="button" value="Coquillettes Sans Gluten"></div>
+        <div class= "Fusilli Sans gluten"><input type="button" value= "Fusilli Sans gluten"></div>
+        <div class= "Lasagne sans gluten"><input  type="button" value= "Lasagne sans gluten"></div>
+        <div class= "Penne Rigate sans gluten"><input type ="button" value= "Penne Rigate sans gluten"></div>
+        <div class= "Spaghetti sans gluten"><input type="button" value= "Spaghetti sans gluten"></div>
+        <div class = "Tagliatelle sans gluten"><input type="button" value= "Tagliatelle sans gluten"></div>
+        <script>
+  const seconde= document.querySelector('.secondes.number')
+const heure= document.querySelector('.heure.number')
+const minutes= document.querySelector('.minutes.number')
+const jour= document.querySelector('.jours.number')
+console.log(seconde,minutes,heure,jour)
+</script>
+
+</body>
+</html>
